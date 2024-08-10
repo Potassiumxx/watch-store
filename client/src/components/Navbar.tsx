b973652f8a716836740e3090351890eb71455f9b
Potassium <brainiacgamer444@gmail.com>
2024-08-10 23:36:32 +0545

    Create custom class in tailwind.config.js for links in navbar and create links in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="flex justify-between px-7 py-7 items-center">
+      <div>
+        <span>Logo</span>
+      </div>
+      <div className="flex gap-2 bg-black text-white rounded-[25px] min-h-[3.2rem] items-center">
+        <span className="navbar-link-style border-r-2">
+          <Link to={"/"}>Home</Link>
+        </span>
+        <span className="navbar-link-style border-r-2">
+          <Link to={"products"}>Collection</Link>
+        </span>
+        <span className="navbar-link-style">
+          <Link to={"about"}>About Us</Link>
+        </span>
+      </div>
+      <div className="flex w-28 items-center justify-center gap-4">
+        <span>Search</span>
+        <span>Cart</span>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from "tailwindcss/plugin";
+
+function customClassName({ addComponents }) {
+  const newComponents = {
+    ".navbar-link-style": {
+      "@apply w-[200px] text-center border-white uppercase font-semibold tracking-wider": {},
+    },
+  };
+  addComponents(newComponents);
+}
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [plugin(customClassName)],
 };
