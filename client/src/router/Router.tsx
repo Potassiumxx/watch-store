bdf0cc86fa138f0ab2fd054b67a3494c3a24e902
Potassium <brainiacgamer444@gmail.com>
2024-08-10 21:19:26 +0545

    Initialise Error page for react router

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       element: <App />,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: "*", element: <ErrorPage /> },
+        { path: "/", element: <Home /> },
+      ],
     },
   ]);
 
