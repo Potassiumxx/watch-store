5e5d829adba2447400790e654f02b0a251f324cf
Potassium <brainiacgamer444@gmail.com>
2024-09-02 23:24:45 +0545

    Add search bar with open and close function in navbar
    Also add cart and user icon.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,74 @@
 import { Link } from "react-router-dom";
+import { IoSearchOutline } from "react-icons/io5";
+import { LiaShoppingCartSolid } from "react-icons/lia";
+import { FaRegUser } from "react-icons/fa6";
+import * as React from "react";
 
 export default function Navbar() {
+  const [openSearchBar, setOpenSearchBar] = React.useState<boolean>(false);
+  const [isSearchBarVisible, setIsSearchBarVisible] = React.useState<boolean>(false);
+  const searchBarRef = React.useRef<HTMLInputElement | null>(null);
+  const searchIconRef = React.useRef<HTMLSpanElement | null>(null);
+
+  function handleOpenSearchBar() {
+    setOpenSearchBar(true);
+    setTimeout(() => {
+      setIsSearchBarVisible(true);
+      searchBarRef.current?.focus();
+    }, 0);
+  }
+
+  function handleCloseSearchBar(event: MouseEvent) {
+    if (
+      searchBarRef.current !== null &&
+      !searchIconRef.current?.contains(event.target as Node) &&
+      !searchBarRef.current?.contains(event.target as Node) &&
+      searchBarRef.current.value.trim().length === 0
+    ) {
+      setIsSearchBarVisible(false);
+      setTimeout(() => {
+        setOpenSearchBar(false);
+      }, 300);
+    }
+  }
+
+  React.useEffect(() => {
+    console.log("run");
+    document.addEventListener("click", handleCloseSearchBar);
+    return () => {
+      document.removeEventListener("click", handleCloseSearchBar);
+      console.log("stop");
+    };
+  }, [openSearchBar]);
+
   return (
-    <div className="flex justify-between px-7 py-7 items-center">
+    <div className="flex flex-col justify-between items-center">
+      <div className="relative flex justify-between w-full py-2 pl-[100px] pr-[100px] bg-[#0E0E0E] text-white items-center">
+        <div>
+          <span className="text-[10px] italic capitalize text-[#898989]">Buy best watches with free shipping & returns</span>
+        </div>
+        {openSearchBar && (
+          <input
+            ref={searchBarRef}
+            className={`${
+              isSearchBarVisible ? "w-[45%]" : "w-0 px-0"
+            } h-6 text-black px-2 outline-1 transition-all duration-300 absolute right-[320px]`}
+            placeholder="Search"
+          />
+        )}
+        <div className="flex items-center w-[200px] justify-between">
+          <span ref={searchIconRef} onClick={handleOpenSearchBar} className="hover:cursor-pointer">
+            <IoSearchOutline size={25} />
+          </span>
+          <span className="flex items-center hover:cursor-pointer">
+            <LiaShoppingCartSolid size={32} />
+            <span className="flex items-center text-[12px] font-bold px-[5px] py-[1px] rounded-[50%] bg-[#f28c26]">999</span>
+          </span>
+          <span className="hover:cursor-pointer">
+            <FaRegUser size={22} />
+          </span>
+        </div>
+      </div>
       <div>
         <Link to={"/"}>
           <img alt="logo" />
