f249b5125e2384ce9c1855e2ce3e16acda1298c3
Potassium <brainiacgamer444@gmail.com>
2024-09-06 20:01:58 +0545

    Use image element instead of css in div to display background image

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
-    <div className="bg-[url('src/assets/images/pexels-pixabay-277319.jpg')] absolute top-[48px] w-full h-full bg-cover bg-center">
+    <div className="h-screen">
+      <img
+        src="./src/assets/images/pexels-pixabay-277319.jpg"
+        alt="Round watch"
+        className="absolute top-[48px] w-full h-full object-cover object-center"
+      />
       <h1>Home Page</h1>
     </div>
   );
