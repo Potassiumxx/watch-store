915fd4614d30531cfa1394a5e60b671e2fffe616
Potassiumxx <brainiacgamer444@gmail.com>
2025-04-22 18:38:55 +0545

    Create watch collection section's component (Collection.tsx)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,35 +2,36 @@
 import * as React from "react";
 import Button from "../../components/Button/Button";
 import { FaCamera } from "react-icons/fa";
+import Collection from "../../components/Collection/Collection";
 
-function ImageSlider() {
-  const imageList = [
-    { src: "./src/assets/images/pexels-pixabay-277319.jpg", alt: "Round Skeleton Watch" },
-    { src: "./src/assets/images/pexels-thefstopper-1075189.jpg", alt: "Black Coloured Chronograph Watch" },
-    { src: "./src/assets/images/pexels-ferarcosn-190819.jpg", alt: "Silver And Black Round Watch" },
-  ];
+// function ImageSlider() {
+//   const imageList = [
+//     { src: "./src/assets/images/pexels-pixabay-277319.jpg", alt: "Round Skeleton Watch" },
+//     { src: "./src/assets/images/pexels-thefstopper-1075189.jpg", alt: "Black Coloured Chronograph Watch" },
+//     { src: "./src/assets/images/pexels-ferarcosn-190819.jpg", alt: "Silver And Black Round Watch" },
+//   ];
 
-  const [currentIndex, setCurrentIndex] = React.useState(0);
+//   const [currentIndex, setCurrentIndex] = React.useState(0);
 
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(() => (currentIndex + 1) % imageList.length);
-    }, 5000);
+//   React.useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentIndex(() => (currentIndex + 1) % imageList.length);
+//     }, 5000);
 
-    return () => clearInterval(interval);
-  }, [currentIndex]);
+//     return () => clearInterval(interval);
+//   }, [currentIndex]);
 
-  return imageList.map((img, index) => (
-    <img
-      key={index}
-      src={img.src}
-      alt={img.alt}
-      className={`absolute top-0 left-0 w-full h-full object-cover object-center transition-opacity duration-1000 ${
-        index === currentIndex ? "opacity-100" : "opacity-0"
-      }`}
-    />
-  ));
-}
+//   return imageList.map((img, index) => (
+//     <img
+//       key={index}
+//       src={img.src}
+//       alt={img.alt}
+//       className={`absolute top-0 left-0 w-full h-full object-cover object-center transition-opacity duration-1000 ${
+//         index === currentIndex ? "opacity-100" : "opacity-0"
+//       }`}
+//     />
+//   ));
+// }
 
 export default function Home() {
   React.useEffect(() => {}, []);
@@ -41,7 +42,7 @@ export default function Home() {
       {/*2nd Photo by Fstopper: https://www.pexels.com/photo/round-black-current-chronograph-watch-with-link-bracelet-1075189/ */}
       {/*3rd Photo by Fernando Arcos:
       https://www.pexels.com/photo/silver-linked-bracelet-silver-and-black-round-chronograph-watch-190819/ */}
-      <div className="absolute inset-0 flex items-end pb-3 min-h-dvh bg-black z-10 backgroundImageStyle">
+      <div className="relative inset-0 flex items-end pb-3 min-h-dvh bg-black z-10 backgroundImageStyle">
         <div className="flex flex-col justify-between text-white px-[50px] min-h-[350px] w-full">
           <div className="self-center max-w-[700px]">
             <div className="flex flex-col gap-7 text-center">
@@ -65,6 +66,9 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <section>
+        <Collection />
+      </section>
     </>
   );
 }
