6fe8e9990c13a8f1e38960be9a3ea9966c917e80
Potassiumxx <brainiacgamer444@gmail.com>
2025-05-12 22:23:30 +0545

    Remove the extra password validation from login form
    Only check if the password is empty or not in the login form because it
    doesn't make sense checking if the password has more than six letters, or
    if it has at least one number and letter, etc in login form.
    
    That belongs in registration form while creating data, not while checking it.

--- client/src/utils/validateForm.ts
+++ client/src/utils/validateForm.ts
@@ -11,12 +11,6 @@ export function validateLoginForm(email: string, password: string): LoginErrors 
 
   if (password.trim().length === 0) {
     errors.password = "Please enter a password";
-  } else if (password.trim().length < 6) {
-    errors.password = "Password must be at least 6 characters";
-  } else if (!/\d/.test(password)) {
-    errors.password = "Password must contain at least one number";
-  } else if (!/[a-zA-Z]/.test(password)) {
-    errors.password = "Password must contain at least one letter";
   }
 
   return errors;
