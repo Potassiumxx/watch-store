9ca61c7aabb4a1e6aba528822b8a030a6cee6dad
Potassiumxx <brainiacgamer444@gmail.com>
2025-05-20 23:33:43 +0545

    Create JSDoc for useForm.ts
    This is to make the code more understandable and to avoid any headaches in future.

--- client/src/hooks/useForm.ts
+++ client/src/hooks/useForm.ts
@@ -20,12 +20,22 @@ interface handleFieldOnChangeParamter<InputFieldType> {
   dirtyField: { [K in keyof InputFieldType]: boolean };
 }
 
+/**
+ *
+ * @param initialState Initial state of form in **object** with keys as **string** and values as **boolean**.
+ * @returns
+ */
 export default function useFormError<T extends { [key: string]: boolean }>(initialState: T) {
   const [generalError, setGeneralError] = React.useState<string | null>();
   const [dirtyField, dispatchDirtyFieldReducer] = useDirtyField<T>(initialState);
 
+  // Used 'V' instead of 'T' for generic type from now on so that it doesn't get confusing since there's a 'T' in parent too
+
   /**
-   * Handles errors like networks and server errors, no route/API found errors, etc. related to backend connection
+   * Handles networks and server errors, no route/API found errors, etc. related to backend connection
+   *
+   * @template V  The shape of the form field data (e.g., LoginFields, RegisterFields, etc.)
+   * @returns Basically object, with `GENERAL_ERROR_KEY` key and `errorMessage` message.
    */
   function handleFormAPIError<V>(error: unknown): APIErrorReturnType<V> {
     const errorMessage = errorHandler(error);
@@ -36,7 +46,14 @@ export default function useFormError<T extends { [key: string]: boolean }>(initi
     return errorMessage as APIErrorReturnType<V>;
   }
 
-  // Used 'V' instead of 'T' for generic type so that it doesn't get confusing since there's a 'T' in parent too
+  /**
+   * Handles form validation error handling.
+   *
+   * @template V The shape of the form field data (e.g., LoginFields, RegisterFields).
+   * @param validationError An object of type V containing field names and their error messages.
+   * @param setError Function to set errors for the form of type V, called only if errors exist.
+   * @returns True if there were validation errors, otherwise false.
+   */
   function isValidationError<V extends { [K in keyof V]: string | undefined }>(
     validationError: V,
     setError: (field: keyof V, message: string) => void
@@ -49,6 +66,14 @@ export default function useFormError<T extends { [key: string]: boolean }>(initi
     return false;
   }
 
+  /**
+   * Handles authentication after successfully submitting the form
+   *
+   * @template V  The shape of the form field data (e.g., LoginFields, RegisterFields, etc.)
+   * @param apiCall Function that sends data to the backend and returns response from the backend.
+   * @param setError Function to set errors for the form of type V, called only if errors exist.
+   * @returns Promise<void>
+   */
   async function handleFormSubmit<V extends { [K in keyof V]: string | undefined }>({
     apiCall,
     setError,
@@ -66,6 +91,23 @@ export default function useFormError<T extends { [key: string]: boolean }>(initi
     }
   }
 
+  /**
+   * Handles field-level updates and conditional validation for form inputs.
+   *
+   * This function is primarily used for "dirty field" validation:
+   * - It runs live field-level validation **only after** a form has already failed validation once.
+   *
+   * @template InputFieldType The shape of the form field data (e.g., LoginFields, RegisterFields, etc).
+   * @param fieldKey Key of InputFieldType (e.g., "email", "password", etc).
+   * @param newValue Value that was changed (e.g., email, password) which is supposed to be updated.
+   * @param allFormValues An object with all fields and values of type `InputFieldType` (e.g., {email: "..", password: ".."}).
+   * @param formValueSetter Function to set value of individual form field (e.g., setLoginEmail, setRegisterEmail, etc).
+   * @param validateFunction Function to validate form (e.g., validateLoginForm, validateRegisterForm, etc).
+   * @param setFieldErrorFunction Function to set error for form fields (e.g., setLoginError, setRegisterError, etc).
+   * @param clearErrorsFunction Function to clear any errors in login form after a successfull validation
+   * @param dirtyField Object to indicate which field has been set with "dirty" (for validation)
+   * @returns
+   */
   function handleFieldOnChange<InputFieldType extends { [K in keyof InputFieldType]: string | undefined }>({
     fieldKey,
     newValue,
