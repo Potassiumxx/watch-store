01beb9a2bb047ec99c76dec8d0b0fb688ce3e455
Potassiumxx <brainiacgamer444@gmail.com>
2025-05-29 13:58:41 +0545

    Create show and hide functionality for password field

--- client/src/components/ui/Input/Input.tsx
+++ client/src/components/ui/Input/Input.tsx
@@ -1,5 +1,8 @@
+import * as React from "react";
 import { useUIStore } from "../../../store/uiStore";
 import { ErrorMessage } from "../Error/ErrorMessage";
+import { VscEye } from "react-icons/vsc";
+import { VscEyeClosed } from "react-icons/vsc";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
@@ -9,13 +12,22 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export default function Input({ label, error, id, className, ...attributes }: InputProps) {
   const isLoading = useUIStore((state) => state.isLoading);
+  const { type, ...restAttributes } = { ...attributes };
+  const isPasswordField: boolean = type === "password";
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState<boolean>(false);
+  const inputType = isPasswordField && isPasswordVisible ? "text" : type;
+
+  function togglePasswordVisible(e: React.MouseEvent): void {
+    e.preventDefault();
+    setIsPasswordVisible(!isPasswordVisible);
+  }
 
   const baseInputClass: string = `bg-black/[0.5] text-white p-2 border-2 border-white/[0.5] rounded-border duration-200 disabled:cursor-not-allowed ${
     error && "border-2 border-red-700"
   } focus:border-[#1bddf3]/[0.7] focus:outline-none focus:ring-0`;
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="relative flex flex-col gap-2">
       <label htmlFor={id} className={`font-semibold text-[14px] uppercase ${error ? "text-red-600" : "text-white"}`}>
         {label} {error && <ErrorMessage message={error} isInputFieldError={true} className="normal-case" />}
       </label>
@@ -24,8 +36,14 @@ export default function Input({ label, error, id, className, ...attributes }: In
         id={id}
         autoComplete="off"
         disabled={isLoading}
-        {...attributes}
+        type={inputType}
+        {...restAttributes}
       />
+      {isPasswordField && (
+        <button className="text-white absolute right-2 bottom-2" onClick={togglePasswordVisible}>
+          {isPasswordVisible ? <VscEyeClosed size={22} /> : <VscEye size={22} />}
+        </button>
+      )}
     </div>
   );
 }
