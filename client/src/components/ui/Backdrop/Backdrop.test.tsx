1410d82db9295661a147ba0e7706ac0928d8d61b
Potassiumxx <brainiacgamer444@gmail.com>
2025-05-29 23:53:03 +0545

    Add few tests for Backdrop component

---
+++ client/src/components/ui/Backdrop/Backdrop.test.tsx
@@ -0,0 +1,28 @@
+import userEvent from "@testing-library/user-event";
+import Backdrop from "./Backdrop";
+import { render, screen } from "@testing-library/react";
+
+describe("Backdrop Component", () => {
+  it("renders when visible", () => {
+    render(<Backdrop isVisible={true} onClose={() => {}} />);
+    const backdrop = screen.getByTestId("backdrop");
+    expect(backdrop).toBeInTheDocument();
+    expect(backdrop).toHaveClass("opacity-100");
+  });
+
+  it("calls onClose function when clicked", async () => {
+    const user = userEvent.setup();
+    const handleClose = vi.fn();
+
+    render(<Backdrop isVisible={true} onClose={handleClose} />);
+    const backdrop = screen.getByTestId("backdrop");
+
+    await user.click(backdrop);
+    expect(handleClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("hides when not visible without interepting clicks", () => {
+    render(<Backdrop isVisible={false} onClose={() => {}} />);
+    expect(screen.getByTestId("backdrop")).toHaveClass("opacity-0");
+  });
+});
