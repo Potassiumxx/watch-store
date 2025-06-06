77a63bd6352e9aa9fb15a410833cb52991e6b8b5
Potassiumxx <brainiacgamer444@gmail.com>
2025-06-06 19:50:36 +0545

    Add JWT_SECRET_KEY env field on EnvConfig
    - Fixes the secret key being null error.
    
    I should have done this earlier, but it worked without adding it in the EnvConfig initially and I forgot to add it later.

--- server/src/main/java/com/watchstore/server/EnvConfig.java
+++ server/src/main/java/com/watchstore/server/EnvConfig.java
@@ -9,5 +9,6 @@ public class EnvConfig {
         System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
         System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
         System.setProperty("app.admin.email", dotenv.get("ADMIN_EMAIL"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
     }
 }
--- server/src/main/java/com/watchstore/server/util/JWTUtil.java
+++ server/src/main/java/com/watchstore/server/util/JWTUtil.java
@@ -10,9 +10,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 public class JWTUtil {
-    private static final String SECRET_KEY = System.getProperty("JWT_SECRET_KEY"); // Has to be at least 256 bits
     
     public static String generateTokenString(UserDTO userDTO) {
+        final String SECRET_KEY = System.getProperty("JWT_SECRET_KEY"); // Has to be at least 256 bits
         Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
 
         return Jwts.builder()
