10212663b4df2328928a6caff7d087849e1069f2
Potassiumxx <brainiacgamer444@gmail.com>
2025-06-06 22:22:42 +0545

    Make input component more flexible for styling
    - Parent, label and input element can now have their own stylings if needed without breaking the whole input styling.
    - Also kind of backward compatible, meaning the previously used input component do not have to change:
    Can work even without the newly added props without breaking the UI.

--- client/src/components/ui/Input/Input.tsx
+++ client/src/components/ui/Input/Input.tsx
@@ -8,9 +8,24 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   error?: string;
   label?: string;
+  parentClassName?: string;
+  labelClassName?: string;
+  inputClassName?: string;
 }
 
-export default function Input({ label, error, id, className, ...attributes }: InputProps) {
+/**
+ *
+ * @param label Label text for input field.
+ * @param error Error text for input field.
+ * @param id represents label "for" and input "id" field.
+ * @param parentClassName className for the parent `div` element that wraps `label` and `input` elements. If none is provided, default styling will be used.
+ * @param labelClassName className for label element's styling. If none is provided, default or provided `className`'s value will be used.
+ * @param inputClassName className for input element's styling. If none is provided, default or provided `className`'s value will be used.
+ *
+ * @ Other **input** field attributes can be provided as well if needed. Currently, attributes for label field is not available.
+ */
+
+export default function Input({ label, error, id, parentClassName, labelClassName, inputClassName, ...attributes }: InputProps) {
   const isLoading = useUIStore((state) => state.isLoading);
   const { type, ...restAttributes } = { ...attributes };
   const isPasswordField: boolean = type === "password";
@@ -22,17 +37,17 @@ export default function Input({ label, error, id, className, ...attributes }: In
     setIsPasswordVisible(!isPasswordVisible);
   }
 
-  const baseInputClass: string = `bg-black/[0.5] text-white p-2 border-2 border-white/[0.5] rounded-border duration-200 disabled:cursor-not-allowed ${
-    error && "border-2 border-red-700"
-  } focus:border-[#1bddf3]/[0.7] focus:outline-none focus:ring-0`;
+  const baseInputClass: string = `bg-black/[0.5] text-white p-2 border-2 border-white/[0.5] rounded-border duration-200 disabled:cursor-not-allowed focus:border-[#1bddf3]/[0.7] focus:outline-none focus:ring-0`;
 
   return (
-    <div className="relative flex flex-col gap-2">
-      <label htmlFor={id} className={`font-semibold text-[14px] uppercase ${error ? "text-red-600" : "text-white"}`}>
+    <div className={parentClassName ?? `relative flex flex-col gap-2`}>
+      <label
+        htmlFor={id}
+        className={`font-semibold text-[14px] uppercase ${labelClassName ?? null} ${error ? "text-red-600" : "text-white"}`}>
         {label} {error && <ErrorMessage message={error} isInputFieldError={true} className="normal-case" />}
       </label>
       <input
-        className={`${baseInputClass} ${className} ${error && "border-2 border-red-800 focus:border-red-600"}`}
+        className={`${baseInputClass} ${inputClassName ?? null} ${error ? "border-2 border-red-800 focus:border-red-600" : null}`}
         id={id}
         autoComplete="off"
         disabled={isLoading}
