7e91b200b84a20ac91d53cc038dcfe6b5b9e4731
Potassiumxx <brainiacgamer444@gmail.com>
2025-06-07 14:04:39 +0545

    Create add product form layout/UI
    - No real handling of product adding yet.
    - Just a basic UI layouy

--- client/src/pages/Profile/admin/AdminProductPage.tsx
+++ client/src/pages/Profile/admin/AdminProductPage.tsx
@@ -1,10 +1,53 @@
+import * as React from "react";
+import Button from "../../../components/ui/Button/Button";
+import Form from "../../../components/ui/Form/Form";
+import Input from "../../../components/ui/Input/Input";
 import ProfileContentContainer from "../container/ProfileContentContainer";
 
 export default function AdminProductPage() {
+  const parentClassStyle = "grid grid-cols-[1fr_2.5fr] gap-10 items-center";
+  const [fileName, setFileName] = React.useState<string | null>(null);
+
+  function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (file) setFileName(file.name);
+  }
+
+  async function handleAddProductSubmit(e) {
+    e.preventDefault();
+    console.log("ok");
+  }
   return (
     <>
       <ProfileContentContainer title="Add Product">
-        <div className="text-white">Products</div>
+        <div className="innerDivBackgroundColour shadow-lg shadow-black rounded-md px-20">
+          <Form handleFormSubmit={handleAddProductSubmit} className="gap-12">
+            <Input id="product-name" label="Product Name" parentClassName={parentClassStyle} placeholder="Rolex" />
+            <Input id="product-price" label="Product Price" parentClassName={parentClassStyle} placeholder="199" />
+            <Input
+              id="product-category"
+              label="Product Category"
+              parentClassName={parentClassStyle}
+              placeholder="Digital Watch"
+            />
+            <Input
+              id="product-description"
+              label="Product Description"
+              parentClassName={parentClassStyle}
+              placeholder="Description of the product"
+            />
+            <Input
+              id="product-image"
+              label="Upload Product Image"
+              parentClassName="flex flex-col justify-center gap-10 mt-5"
+              placeholder="Description of the product"
+              type="file"
+              fileName={fileName}
+              onChange={handleFileUpload}
+            />
+            <Button className="formButtonStyle w-[40%] self-center mt-7" textValue="Submit" />
+          </Form>
+        </div>
       </ProfileContentContainer>
     </>
   );
--- client/src/pages/Profile/container/ProfileContentContainer.tsx
+++ client/src/pages/Profile/container/ProfileContentContainer.tsx
@@ -20,7 +20,7 @@ export default function ProfileContentContainer({ children, title, isLoading }: 
       {isLoading ? (
         <Loader size={20} className="m-auto border-white mt-40" />
       ) : (
-        <div className="flex justify-center mt-20 w-full">{children}</div>
+        <div className="flex justify-center mt-14 w-full">{children}</div>
       )}
     </div>
   );
