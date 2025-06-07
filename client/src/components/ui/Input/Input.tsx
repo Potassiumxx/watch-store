2257dbaa4a9c6be35738a621924e3fea38c0f3fd
Potassiumxx <brainiacgamer444@gmail.com>
2025-06-07 14:00:49 +0545

    Modify the Input component to be usable for file uploads

--- client/src/components/ui/Input/Input.tsx
+++ client/src/components/ui/Input/Input.tsx
@@ -11,6 +11,11 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   parentClassName?: string;
   labelClassName?: string;
   inputClassName?: string;
+  fileName?: string | null;
+}
+
+interface FileNameContainerProps {
+  fileName: string | null | undefined;
 }
 
 /**
@@ -21,16 +26,25 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
  * @param parentClassName className for the parent `div` element that wraps `label` and `input` elements. If none is provided, default styling will be used.
  * @param labelClassName className for label element's styling. If none is provided, default or provided `className`'s value will be used.
  * @param inputClassName className for input element's styling. If none is provided, default or provided `className`'s value will be used.
- *
+ * @param fileName only for inputs with type `file`. This is used to show the file's name after uploading a file.
  * @ Other **input** field attributes can be provided as well if needed. Currently, attributes for label field is not available.
  */
-
-export default function Input({ label, error, id, parentClassName, labelClassName, inputClassName, ...attributes }: InputProps) {
+export default function Input({
+  label,
+  error,
+  id,
+  parentClassName,
+  labelClassName,
+  inputClassName,
+  fileName,
+  ...attributes
+}: InputProps) {
   const isLoading = useUIStore((state) => state.isLoading);
   const { type, ...restAttributes } = { ...attributes };
   const isPasswordField: boolean = type === "password";
   const [isPasswordVisible, setIsPasswordVisible] = React.useState<boolean>(false);
   const inputType = isPasswordField && isPasswordVisible ? "text" : type;
+  const isInputTypeFile = type === "file" && true;
 
   function togglePasswordVisible(e: React.MouseEvent): void {
     e.preventDefault();
@@ -39,15 +53,23 @@ export default function Input({ label, error, id, parentClassName, labelClassNam
 
   const baseInputClass: string = `bg-black/[0.5] text-white p-2 border-2 border-white/[0.5] rounded-border duration-200 disabled:cursor-not-allowed focus:border-[#1bddf3]/[0.7] focus:outline-none focus:ring-0`;
 
+  const fileInputStyle: string =
+    "cursor-pointer border-2 border-white/[0.5] p-2 rounded-border bg-black/[0.5] text-white hover:border-[#1bddf3]/[0.7] duration-200 text-center w-[50%] self-center";
+
   return (
     <div className={parentClassName ?? `relative flex flex-col gap-2`}>
+      {isInputTypeFile && <FileNameContainer fileName={fileName} />}
       <label
         htmlFor={id}
-        className={`font-semibold text-[14px] uppercase ${labelClassName ?? null} ${error ? "text-red-600" : "text-white"}`}>
+        className={`font-semibold text-[14px] uppercase ${isInputTypeFile && fileInputStyle} ${labelClassName ?? null} ${
+          error ? "text-red-600" : "text-white"
+        }`}>
         {label} {error && <ErrorMessage message={error} isInputFieldError={true} className="normal-case" />}
       </label>
       <input
-        className={`${baseInputClass} ${inputClassName ?? null} ${error ? "border-2 border-red-800 focus:border-red-600" : null}`}
+        className={`${isInputTypeFile ? "hidden" : baseInputClass} ${inputClassName ?? null} ${
+          error ? "border-2 border-red-800 focus:border-red-600" : null
+        }`}
         id={id}
         autoComplete="off"
         disabled={isLoading}
@@ -64,5 +86,32 @@ export default function Input({ label, error, id, parentClassName, labelClassNam
         </button>
       )}
     </div>
+  );
+}
+
+/**
+ * @description Component only used when a file is uploaded via input type `file`. It displays the uploaded file name.
+ * @param fileName Name of the uploaded file.
+ */
+function FileNameContainer({ fileName }: FileNameContainerProps): React.ReactNode {
+  function truncateString(filename: string, maxLength: number = 30): string {
+    if (filename.length <= maxLength) return filename;
+
+    const startingIndexOfExtension = filename.lastIndexOf("."); // Extensions starts with dot
+    const name = filename.slice(0, startingIndexOfExtension);
+    const extension = filename.slice(startingIndexOfExtension);
+
+    const stringToDisplay = maxLength - extension.length - 3;
+    const start = Math.ceil(stringToDisplay / 2);
+    const end = Math.floor(stringToDisplay / 2);
+
+    return `${name.slice(0, start)}...${name.slice(name.length - end)}${extension}`;
+  }
+  return (
+    fileName && (
+      <span className="block truncate max-w-[256px] overflow-hidden text-white self-center mt-2">
+        {fileName && truncateString(fileName)}
+      </span>
+    )
   );
 }
