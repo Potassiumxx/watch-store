dcebd9403a1e27caba221e2034e2a458201d1996
Potassiumxx <brainiacgamer444@gmail.com>
2025-06-26 22:43:10 +0545

    Change product DTO variable to match the key value sent from frontend
    - This was important because JsonProperty could not be used for multipart form data.
    
    So, if the object/data sent from frontend had "productName" as key then the variables in the productRequest DTO
    should match that.

--- server/src/main/java/com/watchstore/server/AdminController.java
+++ server/src/main/java/com/watchstore/server/AdminController.java
@@ -3,8 +3,8 @@ package com.watchstore.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +19,16 @@ public class AdminController {
     private ProductRepository productRepository;
 
     @PostMapping("/add-product")
-    public ResponseEntity<Object> addProduct(@RequestBody ProductRequest productRequest) {
+    public ResponseEntity<Object> addProduct(@ModelAttribute ProductRequest productRequest) {
         Product product = new Product();
-        product.setName(productRequest.getName());
-        product.setPrice(productRequest.getPrice());
-        product.setCategory(productRequest.getCategory());
-        product.setDescription(productRequest.getDescription());
-        product.setImage(productRequest.getImage());
+        product.setName(productRequest.getProductName());
+        product.setPrice(productRequest.getProductPrice());
+        product.setCategory(productRequest.getProductCategory());
+        product.setDescription(productRequest.getProductDescription());
+        product.setImage(productRequest.getProductImage());
 
         // productRepository.save(product);
-        System.out.println(product);
+        System.out.println("Product Name: " + productRequest.getProductName());
 
         return new ResponseEntity<>("Finished", HttpStatus.CREATED);
     }
--- server/src/main/java/com/watchstore/server/dto/ProductRequest.java
+++ server/src/main/java/com/watchstore/server/dto/ProductRequest.java
@@ -1,21 +1,21 @@
 package com.watchstore.server.dto;
 
 public class ProductRequest {
-    private String name;
-    private double price;
-    private String category;
-    private String description;
-    private String image;
+    private String productName;
+    private double productPrice;
+    private String productCategory;
+    private String productDescription;
+    private String productImage;
 
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public String getCategory() { return category; }
-    public String getDescription() { return description; }
-    public String getImage() { return image; }
+    public String getProductName() { return productName; }
+    public double getProductPrice() { return productPrice; }
+    public String getProductCategory() { return productCategory; }
+    public String getProductDescription() { return productDescription; }
+    public String getProductImage() { return productImage; }
 
-    public void setName(String name) { this.name = name; }
-    public void setPrice(double price) { this.price = price; }
-    public void setCategory(String category) { this.category = category; }
-    public void setDescription(String description) { this.description = description; }
-    public void setImage(String image) { this.image = image; }
+    public void setProductName(String productName) { this.productName = productName; }
+    public void setProductPrice(double productPrice) { this.productPrice = productPrice; }
+    public void setProductCategory(String productCategory) { this.productCategory = productCategory; }
+    public void setProductDescription(String productDescription) { this.productDescription = productDescription; }
+    public void setProductImage(String productImage) { this.productImage = productImage; }
 }
