0583d00d9a0402bac7f37310af777990eba31e84
Potassiumxx <brainiacgamer444@gmail.com>
2025-07-22 18:20:55 +0545

    Remove unused imports

--- server/src/main/java/com/watchstore/server/controller/AdminController.java
+++ server/src/main/java/com/watchstore/server/controller/AdminController.java
@@ -1,25 +1,14 @@
 package com.watchstore.server.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.watchstore.server.dto.product.ProductRequest;
-import com.watchstore.server.model.Inventory;
-import com.watchstore.server.model.Product;
-import com.watchstore.server.repository.InventoryRepository;
-import com.watchstore.server.repository.ProductRepository;
 import com.watchstore.server.service.ProductService;
-import com.watchstore.server.util.FileStorageUtil;
 
 @RestController
 @RequestMapping("/api/admin")
