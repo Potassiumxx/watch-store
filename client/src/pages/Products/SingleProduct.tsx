7ad8a5b4c1a60a9ca466a20f46c9b4b15f650343
Potassiumxx <brainiacgamer444@gmail.com>
2025-07-27 10:18:34 +0545

    Design the single product page
    - This design is fine for now.

--- client/src/pages/Products/SingleProduct.tsx
+++ client/src/pages/Products/SingleProduct.tsx
@@ -5,6 +5,7 @@ import { getProductByID } from "../../services/api/productAPI";
 import { fetchErrorCatcher } from "../../utils/helpers";
 import Loader from "../../components/ui/Loader/Loader";
 import FetchStatusDisplay from "../../components/ui/FetchStatusDisplay/FetchStatusDisplay";
+import Button from "../../components/ui/Button/Button";
 
 export default function SingleProductPage() {
   const { id } = useParams();
@@ -30,12 +31,33 @@ export default function SingleProductPage() {
   return (
     <FetchStatusDisplay isLoading={isLoading} isEmpty={!product} error={error} emptyMessage="Could not fetch product.">
       {product &&
-        <div className="p-10 text-white">
-          <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
-          <p className="text-lg mb-2">{product.category}</p>
-          <p className="text-base mb-2">{product.description}</p>
-          <p className="text-2xl font-semibold mb-4">${product.price}</p>
-          <img src={`http://localhost:5000/images/${product.imagePath}`} alt={product.name} className="w-[300px] rounded" />
+        <div className="grid grid-cols-2 gap-8 text-white">
+          <img
+            src={`http://localhost:5000/images/${product.imagePath}`}
+            alt={product.name}
+            className="w-[600px] rounded-lg shadow-lg"
+          />
+          <div className="flex flex-col px-2 py-4 gap-8 pl-8 max-w-[500px]">
+            <div>
+              <h1 className="text-5xl font-semibold mb-4">{product.name}</h1>
+              <p className="text-2xl text-gray-300 mb-2">{product.category}</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span className="flex justify-between text-gray-300 items-center uppercase">
+                <h4>Price</h4>
+                <h4>Quantity</h4>
+              </span>
+              <span className="flex justify-between items-center">
+                <p className="text-4xl font-semibold">{product.price}</p>
+                <span className="text-3xl font-semibold text-left">{product.quantity}</span>
+              </span>
+            </div>
+            <span className="flex flex-col gap-4 border-b-2">
+              <h4 className="uppercase text-gray-300">Description</h4>
+              <p className="text-base mb-4 max-h-[300px] overflow-scroll">{product.description}</p>
+            </span>
+            <Button textValue="Add to cart" className="defaultButtonStyle w-full bg-orange-700 hover:bg-orange-600 hover:text-white" />
+          </div>
         </div>
       }
     </FetchStatusDisplay>
