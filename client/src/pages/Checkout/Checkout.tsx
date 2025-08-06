8c2d7d3993fd62cf89f5ba951bde2b034bb41ce1
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-06 10:19:48 +0545

    Create a shipping information form
    - Just a form layout for now.
    
    - I have put the card information fields for now but I am not sure for
      now if I will implement them or not later.

--- client/src/pages/Checkout/Checkout.tsx
+++ client/src/pages/Checkout/Checkout.tsx
@@ -1,4 +1,7 @@
 import Button from "../../components/ui/Button/Button";
+import Form from "../../components/ui/Form/Form";
+import FormFieldWrapper from "../../components/ui/FormFieldWrapper/FormFieldWrapper";
+import Input from "../../components/ui/Input/Input";
 import { useCheckoutStore } from "../../store/checkoutStore"
 
 export default function Checkout() {
@@ -17,7 +20,7 @@ export default function Checkout() {
             checkoutItems.length === 0 ? (
               <div>No items to checkout</div>
             ) : (
-              <div className="pt-10 max-h-[300px] overflow-y-auto"> {
+              <div className="pt-10 max-h-[500px] overflow-y-auto"> {
                 checkoutItems.map((item) => {
                   return (
                     <div className="pt-2" key={item.id}>
@@ -44,15 +47,60 @@ export default function Checkout() {
               </div>
             )
           }
-          <div className="max-w-full whitespace-nowrap overflow-x-auto">
-            <span className="text-white text-2xl">Total: {totalAmount.toFixed(2)}</span>
+          <div className="flex justify-between">
+            <span className="text-white text-2xl">Total</span>
+            <div className="max-w-[200px] overflow-style font-semibold text-2xl">{totalAmount.toFixed(2)}</div>
           </div>
         </div>
       </div>
-      <div className="component-x-axis-padding rounded-sm" style={{
+      <div className="component-x-axis-padding rounded-sm h-[550px]" style={{
         boxShadow: "-3px 0 12px 1px rgb(0, 0, 0, 0.8)",
       }}>
         <h1 className="text-3xl">Shipping Information</h1>
+        <div className="px-0 py-10 flex flex-col justify-between h-full">
+          <div className="flex flex-col gap-4">
+            <FormFieldWrapper
+              id="drop-location"
+              label="Drop Location"
+              error={""}
+            >
+              <Input id="drop-location" error={""} placeholder="Enter pickup location" />
+            </FormFieldWrapper>
+            <FormFieldWrapper
+              id="phone-number"
+              label="Phone Number"
+              error={""}
+            >
+              <Input id="phone-number" error={""} type="tel" />
+            </FormFieldWrapper>
+            <FormFieldWrapper
+              id="card-number"
+              label="Card Number"
+              error={""}
+            >
+              <Input id="card-number" error={""} placeholder="1234 1234 1234 1234" />
+            </FormFieldWrapper>
+            <div className="flex gap-4">
+              <FormFieldWrapper
+                id="expiry"
+                label="Expiry"
+                error={""}
+              >
+                <Input id="expiry" error={""} type="text" placeholder="MM/YY" maxLength={5} />
+              </FormFieldWrapper>
+
+              <FormFieldWrapper
+                id="cvv"
+                label="CVV"
+                error={""}
+              >
+                <Input id="cvv" error={""} type="password" maxLength={4} placeholder="CVV/CVC" />
+              </FormFieldWrapper>
+            </div>
+
+          </div>
+          <Button textValue={`Pay ${totalAmount.toFixed(2)}`} className="defaultButtonStyle w-full mb-4" />
+        </div>
       </div>
     </div>
   )
