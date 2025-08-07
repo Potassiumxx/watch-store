08acba79b54d254ad3c6b14ebc659c1241a5a1d0
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-07 16:09:59 +0545

    Create pagination for order table
    - Display five order list per page. It is not exactly a page but since
      it's called pagination I am going to stick with that name.
    
    - Change a styling again for table (added fixed height and full width).
    
    - Fix the "View your order" link in CheckoutSuccess component. It now
      navigates to the orders page instead of profile page.

--- client/src/pages/Checkout/CheckoutSuccess.tsx
+++ client/src/pages/Checkout/CheckoutSuccess.tsx
@@ -17,7 +17,7 @@ export default function CheckoutSuccess() {
       <h1 className="text-white text-4xl text-center mt-[10rem] mb-20">Checkout successful!</h1>
       <h3 className="text-white text-2xl text-center mb-20">Your product should be with you soon.</h3>
       <div className="w-full text-center">
-        <Link to={"/profile"} className="text-white text-2xl underline underline-offset-8">View your order</Link>
+        <Link to={"/profile/orders"} className="text-white text-2xl underline underline-offset-8">View your order</Link>
       </div>
     </div>
   )
--- client/src/pages/Profile/order/Order.tsx
+++ client/src/pages/Profile/order/Order.tsx
@@ -9,6 +9,14 @@ import { useAuthStore } from "../../../store/authStore";
 export default function Order() {
   const [orders, setOrders] = React.useState<OrderResponseDTO[]>([]);
   const [expandedOrderId, setExpandedOrderId] = React.useState<number | null>(null);
+  const [currentPage, setCurrentPage] = React.useState<number>(1);
+  const itemsPerPage = 5;
+
+  const totalPages = Math.ceil(orders.length / itemsPerPage);
+  const paginatedOrders = orders.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
   const { role, userID } = useUserStore();
 
@@ -50,72 +58,93 @@ export default function Order() {
 
   return (
     <ProfileContentContainer title="View Orders">
-      <table className="text-white min-w-full border border-gray-400 bg-black/[.7] table-fixed">
-        <thead className="text-left text-[15px] text-bold bg-white/[.2]">
-          <tr className="p-4">
-            <th className="p-2">#</th>
-            <th className="p-2">Order ID</th>
-            {role === ROLES.ADMIN && <th className="p-2 w-[400px]">User Email</th>}
-            <th className="p-2">Date (D/M/Y)</th>
-            <th className="p-2">Time (24h)</th>
-            <th className="p-2">Total</th>
-            <th className="p-2 w-[200px]">Action</th>
-          </tr>
-        </thead>
-        <tbody className="text-left">
-          {
-            orders.map((order, index) => {
-              const totalPrice = order.orderItems.reduce((acc, item) => acc + item.unitPrice * item.quantity, 0);
-              return (
-                <React.Fragment key={order.orderID}>
-                  <tr className="border-b border-gray-400">
-                    <td className="p-2">{index + 1}</td>
-                    <td className="p-2">{order.orderID}</td>
-                    {role === ROLES.ADMIN && <td className="p-2 overflow-x-auto">{order.userEmail}</td>}
-                    <td className="p-2">{new Date(order.createdAt).toLocaleDateString()}</td>
-                    <td className="p-2">{new Date(order.createdAt).toLocaleTimeString()}</td>
-                    <td className="p-2">{totalPrice}</td>
-                    <td className="p-2">
-                      <button
-                        onClick={() => toggleShowOrderItems(order.orderID)}
-                        className="border px-4 py-2 hover:bg-white hover:text-black duration-200">
-                        {expandedOrderId === order.orderID ? " Hide Details" : "Show Details"}
-                      </button>
-                    </td>
-                  </tr>
-                  {expandedOrderId === order.orderID && (
-                    <tr>
-                      <td colSpan={role === ROLES.ADMIN ? 9 : 8} className="p-4">
-                        <div className="grid grid-cols-[2fr_1fr]">
-                          <div className="">
-                            <strong>Items:</strong>
-                            <ul className="list-dash list-inside pl-4 mt-1">
-                              {order.orderItems.map((item) => (
-                                <li key={item.productId}>
-                                  {item.productName} | Quantity: {item.quantity} | Price: {item.unitPrice}
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                          <div className="flex flex-col gap-8 ml-auto">
-                            <div className="mb-2">
-                              <strong>Phone Number:</strong> {order.phoneNumber}
-                            </div>
-                            <div className="mb-2">
-                              <strong>Drop Location:</strong> {order.dropLocation}
-                            </div>
-                          </div>
-                        </div>
+      <div className="innerDivBackgroundColour border border-white/[.5] pb-4 rounded-md shadow-lg shadow-black min-h-[420px] flex flex-col justify-between min-w-full">
+        <table className="text-white min-w-full table-fixed">
+          <thead className="text-left text-[15px] text-bold bg-white/[.2]">
+            <tr className="p-4">
+              <th className="p-2">#</th>
+              <th className="p-2">Order ID</th>
+              {role === ROLES.ADMIN && <th className="p-2 w-[400px]">User Email</th>}
+              <th className="p-2">Date (D/M/Y)</th>
+              <th className="p-2">Time (24h)</th>
+              <th className="p-2">Total</th>
+              <th className="p-2 w-[200px]">Action</th>
+            </tr>
+          </thead>
+          <tbody className="text-left">
+            {
+              paginatedOrders.map((order, index) => {
+                const totalPrice = order.orderItems.reduce((acc, item) => acc + item.unitPrice * item.quantity, 0);
+                return (
+                  <React.Fragment key={order.orderID}>
+                    <tr className="border-b border-gray-400">
+                      <td className="p-2">{index + 1}</td>
+                      <td className="p-2">{order.orderID}</td>
+                      {role === ROLES.ADMIN && <td className="p-2 overflow-x-auto">{order.userEmail}</td>}
+                      <td className="p-2">{new Date(order.createdAt).toLocaleDateString()}</td>
+                      <td className="p-2">{new Date(order.createdAt).toLocaleTimeString()}</td>
+                      <td className="p-2">{totalPrice}</td>
+                      <td className="p-2">
+                        <button
+                          onClick={() => toggleShowOrderItems(order.orderID)}
+                          className="border px-4 py-2 hover:bg-white hover:text-black duration-200">
+                          {expandedOrderId === order.orderID ? " Hide Details" : "Show Details"}
+                        </button>
                       </td>
                     </tr>
-                  )}
-                </React.Fragment>
+                    {expandedOrderId === order.orderID && (
+                      <tr>
+                        <td colSpan={role === ROLES.ADMIN ? 9 : 8} className="p-4">
+                          <div className="grid grid-cols-[2fr_1fr]">
+                            <div className="">
+                              <strong>Items:</strong>
+                              <ul className="list-dash list-inside pl-4 mt-1">
+                                {order.orderItems.map((item) => (
+                                  <li key={item.productId}>
+                                    {item.productName} | Quantity: {item.quantity} | Price: {item.unitPrice}
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                            <div className="flex flex-col gap-8 ml-auto">
+                              <div className="mb-2">
+                                <strong>Phone Number:</strong> {order.phoneNumber}
+                              </div>
+                              <div className="mb-2">
+                                <strong>Drop Location:</strong> {order.dropLocation}
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    )}
+                  </React.Fragment>
+                )
+              }
               )
             }
-            )
-          }
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+        <div className="flex justify-center items-center mt-4 gap-4">
+          <button
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
+            className="px-4 py-2 border text-white rounded hover:bg-white disabled:opacity-50"
+          >
+            Previous
+          </button>
+
+          <span className="text-white">Page {currentPage} of {totalPages}</span>
+
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage(currentPage + 1)}
+            className="px-4 py-2 border text-white rounded hover:bg-white disabled:opacity-50"
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </ProfileContentContainer >
   )
 }
